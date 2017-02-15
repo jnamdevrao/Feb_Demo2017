c4213940819a9870c151fe67438d83532e44591e
learning <learning@DIN66006102.corp.capgemini.com>
2017-02-15 17:04:06 +0530

    addedd

--- JavaTest/src/main/java/com/db/Oratest.java
+++ JavaTest/src/main/java/com/db/Oratest.java
@@ -5,7 +5,7 @@ public class Oratest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Ora activatdsfsfed");
+		System.out.println("Ora activatdsfsfvxcvxced");
 
 	}
 
