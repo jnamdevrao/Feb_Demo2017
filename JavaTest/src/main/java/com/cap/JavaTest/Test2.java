644907bbcfb66077a98b4a3aee73129541293f02
learning <learning@DIN66006102.corp.capgemini.com>
2017-02-15 16:43:56 +0530

    OK

---
+++ JavaTest/src/main/java/com/cap/JavaTest/Test2.java
@@ -0,0 +1,11 @@
+package com.cap.JavaTest;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		App.display();
+		for (int i = 0; i < 5; i++)
+			System.out.println("Second Program runnings" + i);
+	}
+
+}
