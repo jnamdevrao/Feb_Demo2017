b9c1d9da7c55693be64d919d033914748a23622f
learning <learning@DIN66006102.corp.capgemini.com>
2017-02-15 17:15:31 +0530

    asdas

--- JavaTest/src/main/java/com/cap/JavaTest/App.java
+++ JavaTest/src/main/java/com/cap/JavaTest/App.java
@@ -10,7 +10,7 @@ public class App
 	public static void display()
 	
 	{
-        System.out.println( "Display invoked" );
+        System.out.println( "Display asdsainvoked" );
 	}
     public static void main( String[] args )
     {
